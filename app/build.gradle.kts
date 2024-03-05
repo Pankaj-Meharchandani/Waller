ea5402a45bb211a975f6490f9a757f737c0fc8cf
Pankaj Meharchandani <pmeharchandani@yahoo.com>
2024-03-05 11:08:41 +0530

    3.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "3.0.3"
+        versionName = "3.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
