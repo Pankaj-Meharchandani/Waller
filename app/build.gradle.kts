d9897d442700a36b7f25e32301d0a13dd8d8f59e
Pankaj Meharchandani <pmeharchandani@yahoo.com>
2024-03-15 00:54:40 +0530

    user experience improved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "3.0.4"
+        versionName = "3.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
