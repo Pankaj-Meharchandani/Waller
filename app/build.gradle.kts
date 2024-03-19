82068e7d5549d52d34abaee5dccab9ac77346af3
Pankaj Meharchandani <pmeharchandani@yahoo.com>
2024-03-19 20:08:56 +0530

    Added Material colors. Now this app supports material you color theming. stability & relibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,16 +29,22 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.kizitonwose.colorpreference:support:1.1.0")
+    implementation("com.github.kizitonwose.colorpreference:core:1.1.0")
+    implementation("com.github.eltos:simpledialogfragments:v3.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("io.github.eltos:simpledialogfragments:3.8.3")
     implementation("com.google.android.material:material")
     implementation("androidx.palette:palette:1.0.0")
-
     // App Center dependencies
     val appCenterSdkVersion = "5.0.4"
     implementation("com.microsoft.appcenter:appcenter-analytics:$appCenterSdkVersion")
