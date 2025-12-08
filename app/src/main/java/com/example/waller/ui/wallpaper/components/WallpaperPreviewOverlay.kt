08b80f0493a975d3333f13bbce6d8a495de066a5
Pankaj-Meharchandani <Pmeharchandani@yahoo.com>
2025-12-08 17:03:44 +0530

    UI2 for it. UI done for portrait, landscape pending functionality not implemented yet

--- app/src/main/java/com/example/waller/ui/wallpaper/components/WallpaperPreviewOverlay.kt
+++ app/src/main/java/com/example/waller/ui/wallpaper/components/WallpaperPreviewOverlay.kt
@@ -46,7 +46,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.waller.R
 import com.example.waller.ui.wallpaper.ApplyDownloadDialog
 import com.example.waller.ui.wallpaper.Wallpaper
