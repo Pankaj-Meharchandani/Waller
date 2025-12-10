0d31fbe621eeddb7400f4be11ce5a33a6e000f1e
Pankaj-Meharchandani <Pmeharchandani@yahoo.com>
2025-12-10 16:04:49 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "1.9"
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
