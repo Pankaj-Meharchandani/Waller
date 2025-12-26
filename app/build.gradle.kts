087d77269c2af874d9c98a7e89bc21bc035b578a
Pankaj-Meharchandani <Pmeharchandani@yahoo.com>
2025-12-26 16:13:32 +0530

    changing page won't reset the customizations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "2.1.5"
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
