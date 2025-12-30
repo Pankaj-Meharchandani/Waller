248dd48874938a2ffce8a0858014abd684350811
Pankaj-Meharchandani <Pmeharchandani@yahoo.com>
2025-12-30 10:49:13 +0530

    about page scroll working

--- app/src/main/java/com/example/waller/ui/settings/AboutScreen.kt
+++ app/src/main/java/com/example/waller/ui/settings/AboutScreen.kt
@@ -23,7 +23,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.OpenInNew
 import androidx.compose.material.icons.automirrored.outlined.Send
@@ -75,6 +77,7 @@ fun AboutScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(horizontal = 16.dp, vertical = 12.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
