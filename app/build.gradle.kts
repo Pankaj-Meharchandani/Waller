db013dfc26b1cbe57b4514c51702614170405d45
Pankaj-Meharchandani <Pmeharchandani@yahoo.com>
2026-01-13 10:40:38 +0530

    haptics settings added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "2.3"
+        versionName = "2.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
