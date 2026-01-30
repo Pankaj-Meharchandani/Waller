5718e6f32cd45b558b3b464c1a757a08e919697d
Pankaj-Meharchandani <Pmeharchandani@yahoo.com>
2026-01-30 16:32:50 +0530

    Updated Nav bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "2.3.1"
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
