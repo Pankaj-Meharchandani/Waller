c99c35ceda75697606e447509368adadd491fa5a
Pankaj-Meharchandani <Pmeharchandani@yahoo.com>
2026-03-11 11:12:58 +0530

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "2.6"
+        versionName = "2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
