8967f93e8d31c966e914a7d9f2d302d6e1496a5d
pankaj-meharchandani <pmeharchandani@yahoo.com>
2026-04-27 00:58:47 +0530

    beta live

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 2
-        versionName = "2.8"
+        versionName = "2.8-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
