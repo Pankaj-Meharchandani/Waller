c459de7f2a61a4c4b3ccb73ac63274ea1040762b
pankaj-meharchandani <pmeharchandani@yahoo.com>
2026-05-31 13:39:06 +0530

    Updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.waller"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.8-beta"
+        versionCode = 3
+        versionName = "2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
