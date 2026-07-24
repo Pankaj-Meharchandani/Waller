7e2487419333bd7aad8bf890297d161dd16764a5
Pankaj Meharchandani <pmeharchandani@yahoo.com>
2026-07-24 12:13:07 +0530

    v3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.example.waller"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
-        versionName = "3.1"
+        versionCode = 6
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
